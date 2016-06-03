3dd3b85c1d82a423d9fda9967734b1e02e2ab50c
Graeme Mathieson <mathie@woss.name>
2016-06-03 06:54:09 +0100

    Oops, didn't mean to leave those puts in there.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,8 @@
 begin
   require 'codeclimate-test-reporter'
   CodeClimate::TestReporter.start
-  puts "CodeClimate loaded."
 rescue LoadError
   # Don't worry if Code Climate isn't around -- it's just wanted on CI really.
-  puts "CodeClimate not loaded."
 end
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
